aeba37b7a0b5220dc43ff680e56d0d03bbcf0fa2
crismodev <crismodev@gmail.com>
2026-08-30 12:27:16 +0200

    Update generador_contraseña.py

--- generador_contraseña.py
+++ generador_contraseña.py
@@ -1,10 +1,19 @@
-import random
-cuantos_caracteres = input("¿Cuantos caracteres quieres?")
-cuantos_caracteres = int(cuantos_caracteres)
-caracteres = "ABCDabcd0123456789*$%&"
-contraseña_completa = ""
-
-for car in range(cuantos_caracteres):
-    random_caracteres = random.randint(0, len(caracteres) - 1)
-    contraseña_completa = contraseña_completa + (caracteres[random_caracteres])
-print(contraseña_completa)
+import secrets
+cuantos_caracteres = input("¿Cuantos caracteres quieres en tu contraseña?: ")
+try:    
+    caracteres_int = (int(cuantos_caracteres))
+    caracteres = "ABCDabcd0123456789*$%&"
+    caja_caracteres = ""
+    if caracteres_int < 1:
+        print ("La contraseña debe tener por lo menos 1 caracter")
+    elif caracteres_int > 100:
+        print ("La contraseña no puede tener mas de 100 caracteres")
+    else:
+        print("----GENERANDO CONTRASEÑA----")
+        for car in range (caracteres_int):
+            numeros = secrets.randbelow(len(caracteres))
+            caja_caracteres = caja_caracteres + (caracteres[numeros])
+        print(caja_caracteres)
+except:
+    print(f"{cuantos_caracteres} No es un caracter valido")
+    exit()
